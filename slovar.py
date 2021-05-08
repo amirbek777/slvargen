f3c3efbceffdc3a4639b662158b577fe0835ee5f
amirbek777 <74849808+amirbek777@users.noreply.github.com>
2021-05-08 22:50:06 +0600

    Update slovar.py

--- slovar.py
+++ slovar.py
@@ -1,15 +1,21 @@
+import time 
 import random
 import sys
 import os
 import pyfiglet 
 from pyfiglet import Figlet
 
+named_tuple = time.localtime() # получить struct_time
+time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+
+print("Дата и время:", time_string)
+
 font_text = Figlet(font="slant")
 print(font_text.renderText("FireBrut"))
 chars = '1234567890'
 number = input('количество паролей?'+ "\n")
 length = input('длина пароля?'+ "\n")
-number = int(number)
+number = int(number) 
 length = int(length)
 for n in range(number):
     password =''
